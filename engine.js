03465bd87699addaf502bb43aed45aa7825f1901
slash <slashdottriangle@gmail.com>
2023-02-12 22:10:45 +0000

    added information about the resources I used to make the engine

---
+++ engine.js
@@ -0,0 +1,4 @@
+//This JavaScript game engine is based on the work of "danielstuts" and "The Coding Train" (and also the Mozilla Developer Network documentation).
+//The playlists I used :
+//"danielstuts" playlist : https://youtube.com/playlist?list=PLo6lBZn6hgca1T7cNZXpiq4q395ljbEI_
+//"The Coding Train" playlist : https://youtube.com/playlist?list=PLRqwX-V7Uu6ZV4yEcW3uDwOgGXKUUsPOM
