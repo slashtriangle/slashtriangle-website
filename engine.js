382b3713e2892e9a329a1a28a9820451e73a0fb6
slash <slashdottriangle@gmail.com>
2023-02-13 22:26:36 +0000

    added canvas test condition

--- engine.js
+++ engine.js
@@ -2,3 +2,15 @@
 //The playlists I used :
 //"danielstuts" playlist : https://youtube.com/playlist?list=PLo6lBZn6hgca1T7cNZXpiq4q395ljbEI_
 //"The Coding Train" playlist : https://youtube.com/playlist?list=PLRqwX-V7Uu6ZV4yEcW3uDwOgGXKUUsPOM
+
+let canvas_width = 800;
+let canvas_height = 800;
+
+const canvas = document.getElementById("canvas");
+
+if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+} else {
+    console.log("canvas-unsupported");
+}
